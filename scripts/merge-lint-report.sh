28abcf96e2a87cc5a9ee7dba7d2e449b6c97955a
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-06-19 19:19:11 +0700

    delete merge lint

--- scripts/merge-lint-report.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./build/reports/lint
-find . -type f -regex ".*/build/reports/.*xml" -exec cp --parents {} ./build/reports/lint/ \;
-./scripts/merge_report_files.rb ./build/reports/lint/ issue ./build/reports/lint/lint-report.xml
