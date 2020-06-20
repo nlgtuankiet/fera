833650e55e9dc58bb2f1b35b7840b2ebd4e3db75
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-06-20 22:38:29 +0700

    style recent card

--- home/src/main/java/com/nlgtuankiet/fera/home/DataBindingAdapter.kt
+++ home/src/main/java/com/nlgtuankiet/fera/home/DataBindingAdapter.kt
@@ -14,7 +14,7 @@ fun mediaImage(imageView: ImageView, resource: String, whiteOverlay: Boolean) {
   imageView.imageLoad(resource) {
     scaleType = ScaleType.CenterCrop
     // TODO ui 12dp?
-    addTransformer(RoundedCornersTransformer(roundingRadius = imageView.context.pxOf(12)))
+    addTransformer(RoundedCornersTransformer(roundingRadius = imageView.context.pxOf(8)))
     if (whiteOverlay) {
       addTransformer(ColorFilterTransformer(color = ContextCompat.getColor(imageView.context, R.color.home_square_media_more_overlay)))
     }
