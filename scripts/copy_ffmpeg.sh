036bd72bbff1805f1545fdcd182769d1fd49b2aa
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-08-23 15:05:41 +0700

    delete libcpufeatures.so

--- scripts/copy_ffmpeg.sh
+++ scripts/copy_ffmpeg.sh
@@ -22,28 +22,25 @@ copyFfmpeg() {
   cp "${ffmpegSourceDir}/prebuilt/android-arm/ffmpeg/bin/ffprobe" "${jniLibsDir}/armeabi-v7a/libffprobe.so"
   rm -rf "${ffmpegSourceDir}/prebuilt/android-arm/neon"
   copySo "$ffmpegSourceDir" "android-arm" "armeabi-v7a"
-  cp "${ffmpegSourceDir}/android/libs/armeabi-v7a/libcpufeatures.so" "${jniLibsDir}/armeabi-v7a/libcpufeatures.so"
 
   mkdir -p "${jniLibsDir}/arm64-v8a"
   cp "${ffmpegSourceDir}/prebuilt/android-arm64/ffmpeg/bin/ffmpeg" "${jniLibsDir}/arm64-v8a/libffmpeg.so"
   cp "${ffmpegSourceDir}/prebuilt/android-arm64/ffmpeg/bin/ffprobe" "${jniLibsDir}/arm64-v8a/libffprobe.so"
   copySo "$ffmpegSourceDir" "android-arm64" "arm64-v8a"
-  cp "${ffmpegSourceDir}/android/libs/arm64-v8a/libcpufeatures.so" "${jniLibsDir}/arm64-v8a/libcpufeatures.so"
 
   mkdir -p "${jniLibsDir}/x86"
   cp "${ffmpegSourceDir}/prebuilt/android-x86/ffmpeg/bin/ffmpeg" "${jniLibsDir}/x86/libffmpeg.so"
   cp "${ffmpegSourceDir}/prebuilt/android-x86/ffmpeg/bin/ffprobe" "${jniLibsDir}/x86/libffprobe.so"
   copySo "$ffmpegSourceDir" "android-x86" "x86"
-  cp "${ffmpegSourceDir}/android/libs/x86/libcpufeatures.so" "${jniLibsDir}/x86/libcpufeatures.so"
 
   mkdir -p "${jniLibsDir}/x86_64"
   cp "${ffmpegSourceDir}/prebuilt/android-x86_64/ffmpeg/bin/ffmpeg" "${jniLibsDir}/x86_64/libffmpeg.so"
   cp "${ffmpegSourceDir}/prebuilt/android-x86_64/ffmpeg/bin/ffprobe" "${jniLibsDir}/x86_64/libffprobe.so"
   copySo "$ffmpegSourceDir" "android-x86_64" "x86_64"
-  cp "${ffmpegSourceDir}/android/libs/x86_64/libcpufeatures.so" "${jniLibsDir}/x86_64/libcpufeatures.so"
 
   ls -lha data/src/main/jniLibs/arm64-v8a
 }
 
 $1 $2
+
 
